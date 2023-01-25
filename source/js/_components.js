f5ff52c2535bb26c0cca5e0942d6a13877f6cd9e
Roman <romanshliakhov@gmail.com>
2023-01-25 18:54:12 +0200

    start project

---
+++ source/js/_components.js
@@ -0,0 +1,17 @@
+import './components/sliders.js';
+import './components/select.js';
+import './components/burger.js';
+import './components/accordion.js';
+import './components/modals.js';
+import './components/replaceEl.js';
+import './components/phoneMask.js';
+import './components/tabs.js';
+import './components/validateTextarea.js';
+
+
+
+
+
+
+
+
